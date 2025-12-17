7c85e7246259948b5a7df2a6a8e796a17c834ac8
GiulianoDami <38913937+GiulianoDami@users.noreply.github.com>
2025-12-17 14:15:34 +0100

    Updating sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO news_topics (topic, category, date, summary) VALUES
+('New open-earbuds offer head-tracked Dolby Atmos', 'Technology', '2025-03-15', 'Cleer''s new open-ears promise superb spatial audio in their Atmos edition.'),
+('Cloudflare report reveals global internet traffic grew 19% in 2025 - but a lot of it was just bots', 'Cybersecurity', '2025-03-14', 'The report shows that while internet traffic has increased, much of it is caused by automated bots and spam.'),
+('UK MPs target VPNs in latest Online Safety Act debate', 'Cybersecurity', '2025-03-13', 'The debate around age verification and VPNs heats up as MPs confirm their commitment to review the role these providers should have in preventing misuse.');
+
